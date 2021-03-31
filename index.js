fad13f9afa65f4898f5c24d2c7ec70d08ee996be
shifatreza <shifatreza44@gmail.com>
2021-03-31 12:39:10 +0530

    data read

--- index.js
+++ index.js
@@ -29,12 +29,22 @@ client.connect(err => {
         })
     })
 
+    app.get('/products', (req, res) => {
+        productCollection.find({})
+        .toArray((err, items) => {
+            console.log(err);
+            res.send(items);
+            console.log('form database', items);
+
+        })
+    })
+
     // client.close();
 });
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Working on bazar-bag')
 })
 
 app.listen(port, () => {
